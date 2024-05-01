425c4c6c1bcb4231dac54ab3a8bf54ffa80d83c5
harsh <hkmmishra18@gmail.com>
2024-05-01 18:00:45 +0530

    usermodel and videomodel

--- src/utils/ApiResponse.ts
+++ src/utils/ApiResponse.ts
@@ -8,7 +8,7 @@ class ApiResponse {
     this.statusCode = statusCode;
     this.data = data;
     this.message = message;
-    this.success = statusCode < 400;
+    this.success = statusCode < 300;
   }
 }
 
