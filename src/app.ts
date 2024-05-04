7ebb1c31dd8922570a3f6ee68f73aad0fd961ea6
harsh <hkmmishra18@gmail.com>
2024-05-04 21:45:02 +0530

    controllers and routes

--- src/app.ts
+++ src/app.ts
@@ -16,4 +16,10 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//routes import
+import userRoute from "./routes/user.routes";
+
+//routes use
+app.use("/api/v1/users", userRoute);
+
 export default app;
